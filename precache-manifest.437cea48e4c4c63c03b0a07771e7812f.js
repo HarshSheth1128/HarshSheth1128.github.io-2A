d8df7a9403baf9c4d56855355ba9f43a377193bd
Harsh Sheth <nyanman28@gmail.com>
2018-12-31 21:41:18 -0500

    Updates

---
+++ precache-manifest.437cea48e4c4c63c03b0a07771e7812f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "01f029a97a9ab2a713e0f416da35f1bb",
+    "url": "/static/media/weather.01f029a9.png"
+  },
+  {
+    "revision": "27a0981cd9957274cb0a",
+    "url": "/static/css/main.1a56a3c8.chunk.css"
+  },
+  {
+    "revision": "373dc2cc41ebca545d83",
+    "url": "/static/js/1.373dc2cc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ba2b86161a8c482a5ffa4ddc1db7cec7",
+    "url": "/static/media/focus.ba2b8616.png"
+  },
+  {
+    "revision": "27a0981cd9957274cb0a",
+    "url": "/static/js/main.27a0981c.chunk.js"
+  },
+  {
+    "revision": "8f42a825b6e4b88a58a0d7d84dbc1cf6",
+    "url": "/static/media/joke.8f42a825.png"
+  },
+  {
+    "revision": "3f69354f83d176f539955b2332cc1156",
+    "url": "/static/media/npm.3f69354f.png"
+  },
+  {
+    "revision": "9e850ebd9dd9413943426d0c9d69cfae",
+    "url": "/static/media/reddit.9e850ebd.png"
+  },
+  {
+    "revision": "a53b50f73a60df6d6d95d5c130c19a79",
+    "url": "/static/media/ball.a53b50f7.png"
+  },
+  {
+    "revision": "6ad9dca10639d31cc2924d9e73e83a13",
+    "url": "/index.html"
+  }
+];

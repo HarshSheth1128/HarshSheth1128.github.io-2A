69b49ba03be8dd927f8f0b39f364d8bee812499b
Harsh Sheth <nyanman28@gmail.com>
2018-12-31 20:26:13 -0500

    Finished website

--- src/ProjectComponent.jsx
+++ src/ProjectComponent.jsx
@@ -19,14 +19,14 @@ class ProjectComponent extends Component {
                 title={"Focus Web Application"}
                 technologies={"(Javascript, HTML, CSS, React.js)"}
                 description={"The focus application allows you to consolidate all your studying needs in one place. Work without distractions using the built in pomodoro section, tasks tracker (with time), and a notes section"}
-                projectLink={"test"}
+                projectLink={"https://harshsheth1128.github.io/focus"}
                 link={"https://github.com/HarshSheth1128/Focus"}></Project>
                 <Project 
                 image={weatherImg} 
                 title={"Weather Web Application"}
                 technologies={"(Javascript, HTML, CSS, P5.js, AJAX, REST)"}
                 description={"This Weather Web app was the first application I made! It allowed me to learn how to query the Open Weather Map api and use the P5.js library to draw to the screen."}
-                projectLink={"test"}
+                projectLink={"https://harshsheth1128.github.io/Weather/App/"}
                 link={"https://github.com/HarshSheth1128/Weather"}></Project>
                 <Project 
                 image={pythonImg} 
@@ -39,7 +39,7 @@ class ProjectComponent extends Component {
                 title={"Quick Jokes Chrome Extension"}
                 technologies={"(Chrome, Javascript, AJAX, REST)"}
                 description={"I designed this script to poll reddit.com/r/funny and get me new jokes whenever I wanted"}
-                projectLink={"test"}
+                projectLink={"https://chrome.google.com/webstore/detail/joke-extension/jjbkhccadjacoagfdkckilefbjjmfhge?authuser=1"}
                 link={"https://github.com/HarshSheth1128/Joke-Extension"}></Project>
                 <Project 
                 image={npmImg} 
@@ -52,14 +52,14 @@ class ProjectComponent extends Component {
                 title={"Reddit Quick Post"}
                 technologies={"(Javascript, Chrome)"}
                 description={"I designed this extension so that you could quickly procrastinate and view the top,hot, or most recent posts on reddit."}
-                projectLink={"test"}
+                projectLink={"https://chrome.google.com/webstore/detail/reddit-quick-post/nclmpfflbahgmigkndhblfbgdimnhfmc?hl=en"}
                 link={"https://github.com/HarshSheth1128/Reddit-Quick-Post"}></Project>
                 <Project 
                 image={ballImg} 
                 title={"Android Ball Catch Game"}
                 technologies={"(Android, Java)"}
                 description={"I made this game as my introduction to the android environment. It is available on the google play store."}
-                projectLink={"test"}
+                projectLink={"https://play.google.com/store/apps/details?id=com.harsh.game"}
                 link={"https://github.com/HarshSheth1128/Android-Applications"}></Project>
             </div>
         </div>        
--- src/SideBar.jsx
+++ src/SideBar.jsx
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import "./SideBar.css"
+import {Link} from 'react-router-dom';
 
 class SideBar extends Component {
   render() {
     return (
       <div className="SideBar">
         <p id="Name">Harsh Sheth</p>
-        <p id="Bio">This is my bio</p>
+        <p id="Bio">Passionate about all things technology</p>
         <div className="anchorContainer">
-          <a href="/about">About Me</a>
+          <Link to="/about">About Me</Link>
         </div>
         <div className="anchorContainer">
-          <a href="/projects">Projects</a>
+          <Link to="/projects">Projects</Link>
         </div>
         <div className="anchorContainer">
-          <a href="/contact">Contact Me</a>
+          <Link to="/contact">Contact Me</Link>
         </div>
       </div>
     );
